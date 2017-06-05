9ddc77dcbe1593a48a9e27f7d6449a86daee1b2a
cklogic <chuanzyz@sina.com>
2017-06-06 00:46:55 +0800

    mongodb

--- api/src/main/java/com/uubian/api/domain/Message.java
+++ api/src/main/java/com/uubian/api/domain/Message.java
@@ -1,35 +1,40 @@
 package com.uubian.api.domain;
 
 public class Message {
-	private int status;
+	private int code;
 	private String msgText;
 	
 	public Message(){
-		status=0;
+		code=0;
 		msgText="";
 	}
 	
-	public int getStatus() {
-		return status;
+
+	public int getCode() {
+		return code;
 	}
-	public void setStatus(int status) {
-		this.status = status;
+
+
+	public void setCode(int code) {
+		this.code = code;
 	}
+
+
 	public String getMsgText() {
 		return msgText;
 	}
 	public void setMsgText(String msgText) {
 		this.msgText = msgText;
 	}
-	public static Message init(int status,String msgText){
+	public static Message init(int code,String msgText){
 		Message msg = new Message();
 		msg.setMsgText(msgText);
-		msg.setStatus(status);
+		msg.setCode(code);
 		return msg;
 	}
-	public static Message init(int status){
+	public static Message init(int code){
 		Message msg = new Message();
-		msg.setStatus(status);
+		msg.setCode(code);
 		return msg;
 	}
 	public static Message init(String msgText){
