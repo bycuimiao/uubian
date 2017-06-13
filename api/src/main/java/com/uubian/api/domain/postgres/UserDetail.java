3be1f6c7a5c9e8a53707053fc9600e40f5870683
apple <apple@192.168.43.83>
2017-06-13 17:09:49 +0800

    userdetal

---
+++ api/src/main/java/com/uubian/api/domain/postgres/UserDetail.java
@@ -0,0 +1,11 @@
+package com.uubian.api.domain.postgres;
+
+public class UserDetail {
+	private long user_id;
+	private String realname;
+	private String domicile_country;
+	private String domicile_province;
+	private String domicile_city;
+	private String domicile_county;
+	
+}
